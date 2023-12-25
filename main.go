5ff380d12e99dcc4da48b5edce8ae76a8cc30fa7
leonardomdeoli <leonardomdeoli@gmail.com>
2023-12-25 11:25:12 -0300

    Mudando arquivo

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Argo</h1>"))
+		w.Write([]byte("<h1>Hello Argo teste</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
